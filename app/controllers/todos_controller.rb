9a4c96a8f40bdc2a174973fc26f9aa3c6c26ca1c
Jose Tomas <josetomascodecido@gmail.com>
2018-07-26 21:35:44 -0400

    changes about css

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,33 +1,29 @@
 class TodosController < ApplicationController
+  before_action :find_todo, only: [:show, :edit, :update, :destroy, :complete]
   def index
     @todos = Todo.all
   end
   def new
     @todo = Todo.new
   end
-  def create
-    @todo = Todo.new(todo_params)
-    @todo.save
-    redirect_to todos_path
+    def create
+      @todo = Todo.new(todo_params)
+      @todo.save
+      redirect_to todos_path
   end
   def show
-    @todo = Todo.find(params[:id])
   end
   def edit
-    @todo = Todo.find(params[:id])
   end
   def update
-    @todo = Todo.find(params[:id])
     @todo.update(todo_params)
     redirect_to todos_path
   end
   def destroy
-    @todo = Todo.find(params[:id])
     @todo.destroy
     redirect_to todos_path
   end
   def complete
-    @todo = Todo.find(params[:id])
     @todo.completed= true
     @todo.save
     redirect_to todos_path
@@ -39,5 +35,8 @@ class TodosController < ApplicationController
   private
   def todo_params
     params.require(:todo).permit(:description, :completed)
+  end
+  def find_todo
+    @todo = Todo.find(params[:id])
   end
 end
